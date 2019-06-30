4c7d631364c4cccb29cbfe63c9a000c7b9d5d8eb
Dexter Cooke <dexter.cooke@thebridgecorp.com>
2019-06-30 07:29:55 -0400

    Create db

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123@localhost:3306/dex"
-    Debug = True
+    debug = True
--- main.py
+++ main.py
@@ -10,5 +10,21 @@ db = SQLAlchemy(app)
 def home():
     return '<h1>Hello World!</h1>'
 
+
+class User(db.Model):
+    __table__name = 'user_table_name'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'".format(self.username)
+
+
+
 if __name__ == '__main__':
     app.run()
